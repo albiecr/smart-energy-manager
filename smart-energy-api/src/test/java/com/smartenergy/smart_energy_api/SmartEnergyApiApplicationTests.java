b3bfee57a08d59b98a97554431ff6736f7b963e0
albiecr <albertinacrodrigues@gmail.com>
2026-01-15 14:51:03 -0300

    feat: initialize smart energy API with Maven wrapper and Spring Boot setup

---
+++ smart-energy-api/src/test/java/com/smartenergy/smart_energy_api/SmartEnergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartenergy.smart_energy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
