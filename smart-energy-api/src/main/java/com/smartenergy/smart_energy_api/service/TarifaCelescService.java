792281eea82d073a50d29cb72b060fa1e47d187b
albiecr <albertinacrodrigues@gmail.com>
2026-01-29 17:28:11 -0300

    feat: implement TarifaCelescService for peak hour tariff calculation

---
+++ smart-energy-api/src/main/java/com/smartenergy/smart_energy_api/service/TarifaCelescService.java
@@ -0,0 +1,34 @@
+package com.smartenergy.smart_energy_api.service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Service;
+
+import com.smartenergy.smart_energy_api.repository.HotelReadingRepository;
+
+
+@Service
+public class TarifaCelescService {
+    private final HotelReadingRepository repository;
+
+    public static final LocalTime tarifaPontaInicio = LocalTime.of(18, 0); // 18:00
+    public static final LocalTime tarifaPontaFim = LocalTime.of(21, 0);   // 21:00
+
+    public TarifaCelescService(HotelReadingRepository repository) {
+        this.repository = repository;
+    }
+
+    public boolean isHorarioPonta(LocalDateTime dataHora){
+        DayOfWeek diaSemana = dataHora.getDayOfWeek();
+        LocalTime hora = dataHora.toLocalTime();
+
+        if (diaSemana == DayOfWeek.SATURDAY || diaSemana == DayOfWeek.SUNDAY) {
+            return false;
+        }
+
+        return hora.isAfter(tarifaPontaInicio) && hora.isBefore(tarifaPontaFim);
+        }
+
+}
